8a8035866308e22669e3a0a4a82a3a44310a4b59
Micfri <44413144+mikefri@users.noreply.github.com>
2026-05-07 17:36:31 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -10,12 +10,13 @@ import {
 
 // ─── REMPLACE PAR TA CONFIG FIREBASE ──────────────────────────────────────────
 const firebaseConfig = {
-    apiKey:            "TA_API_KEY",
-    authDomain:        "TON_PROJET.firebaseapp.com",
-    projectId:         "TON_PROJET",
-    storageBucket:     "TON_PROJET.appspot.com",
-    messagingSenderId: "TON_SENDER_ID",
-    appId:             "TON_APP_ID"
+  apiKey: "AIzaSyANf8hNGIRryPmZytIxIQ4uDhY6fR6uDKM",
+  authDomain: "teslatube-560c0.firebaseapp.com",
+  projectId: "teslatube-560c0",
+  storageBucket: "teslatube-560c0.firebasestorage.app",
+  messagingSenderId: "1019331471126",
+  appId: "1:1019331471126:web:29beb2914436836bd41237",
+  measurementId: "G-K05WJMWGGH"
 };
 // ──────────────────────────────────────────────────────────────────────────────
 
