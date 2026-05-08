02c55247fb398490c60d3b58a3f0925b61568cef
Micfri <44413144+mikefri@users.noreply.github.com>
2026-05-08 07:30:42 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    TESLATUBE — service-worker.js
 ════════════════════════════════════════════════════ */
 
-const CACHE_NAME    = 'teslatube-v2.1';
+const CACHE_NAME    = 'teslatube-v2.2';
 const STATIC_ASSETS = [
     '/teslatube/',
     '/teslatube/index.html',
