5660b684ee35e6bd35e4ccc3959eedd6addc728a
Micfri <44413144+mikefri@users.noreply.github.com>
2026-05-08 06:48:26 +0200

    Update pwa.js

--- pwa.js
+++ pwa.js
@@ -6,7 +6,7 @@
 /* ── Enregistrement du Service Worker ── */
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('./service-worker.js')
             .then(reg => {
                 console.log('[PWA] Service Worker enregistré :', reg.scope);
 
