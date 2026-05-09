b80cb9f1cb947289bcea4f597aef44f9bc7d2530
Micfri <44413144+mikefri@users.noreply.github.com>
2026-05-09 20:03:10 +0200

    Update cache name to teslatube-v2.9

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    TESLATUBE — service-worker.js
 ════════════════════════════════════════════════════ */
 
-const CACHE_NAME    = 'teslatube-v2.8';
+const CACHE_NAME    = 'teslatube-v2.9';
 const STATIC_ASSETS = [
     '/teslatube/',
     '/teslatube/index.html',
